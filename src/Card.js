9563e80d611efb491231501efb288b25a5c49714
Nkosi Phillip <internap627@gmail.com>
2019-09-02 02:15:19 +0100

    price formatting

--- src/Card.js
+++ src/Card.js
@@ -4,14 +4,18 @@ import './Card.css';
 const Card = ({ product }) => {
   const size = product.size;
   const faceSize = size + "px";
-  const price = product.price;
+  const price = product.price / Math.pow(10, 2);
+  const displayPrice = new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(price);
+  
+  
   return (
     <Fragment >
       {!product.url ? 
       <div className='Card'>
       <h2>Size: {size}</h2>
       <h2>Face: <span style={{fontSize: faceSize}}>{product.face}</span></h2>
-      <h2>Price: {price}</h2>
+      <h2>Price: {displayPrice}</h2>
+      <h2>Date: {}</h2>
       </div>
        : 
        <div className='Card'>
