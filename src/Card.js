42fccb77327e2e7afb3335f113f3f46ed18b1a39
Nkosi Phillip <internap627@gmail.com>
2019-09-02 03:20:55 +0100

    date formatting

--- src/Card.js
+++ src/Card.js
@@ -7,15 +7,44 @@ const Card = ({ product }) => {
   const price = product.price / Math.pow(10, 2);
   const displayPrice = new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(price);
   
+
+  const formatDate = val => {
+    function nth(d) {
+      if (d > 3 && d < 21) return 'th'; 
+      switch (d % 10) {
+        case 1:  return "st";
+        case 2:  return "nd";
+        case 3:  return "rd";
+        default: return "th";
+      }
+    }
+    const months = ['January','February','March','April','May','June','July','August','September','October','November','December'];
+    const today = new Date();
+    const createdOn = new Date(val);
+    const msInDay = 24 * 60 * 60 * 1000;
+    const year = createdOn.getFullYear();
+    const month = months[createdOn.getMonth()];
+    const day = createdOn.getDate();
+    createdOn.setHours(0,0,0,0);
+    today.setHours(0,0,0,0)
+    console.log(createdOn)
+    var diff = (+today - +createdOn)/msInDay
+    if (diff < 7) {
+      return `${diff} days ago`
+    }
+    return `${day}${nth(day)} ${month} ${year}`;
+  };
+
+  const dateString = formatDate(product.date)
   
   return (
     <Fragment >
       {!product.url ? 
       <div className='Card'>
-      <h2>Size: {size}</h2>
-      <h2>Face: <span style={{fontSize: faceSize}}>{product.face}</span></h2>
-      <h2>Price: {displayPrice}</h2>
-      <h2>Date: {}</h2>
+      <h3>Size: {size}</h3>
+      <h3>Face: <span style={{fontSize: faceSize}}>{product.face}</span></h3>
+      <h3>Price: {displayPrice}</h3>
+      <h3>Date: {dateString}</h3>
       </div>
        : 
        <div className='Card'>
